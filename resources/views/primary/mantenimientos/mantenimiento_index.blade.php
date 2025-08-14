fbe61de8fc86df9fd3f540b5763db1e531476bb8
Claudiagradizunah <claudiagradiz@unah.hn>
2025-08-13 23:30:52 -0600

    29 Caso de prueba N°549 Claudia

--- resources/views/primary/mantenimientos/mantenimiento_index.blade.php
+++ resources/views/primary/mantenimientos/mantenimiento_index.blade.php
@@ -11,7 +11,7 @@
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <h1 class="card-title" style="font-size: 30px; margin: 0;">Lista de mantenimientos</h1>
                                 <!-- Botón agregar mantenimiento -->
-                                <a href="{{ route('mantenimientos.create') }}" class="btn btn-primary btn-sm d-flex align-items-center" style="border-radius: 5px; height: 40px; padding: 0 15px;">Agregar Mantenimiento</a>
+                                <a href="{{ route('mantenimientos.create') }}" class="btn btn-primary btn-sm d-flex align-items-center" style="border-radius: 5px; height: 40px; padding: 0 15px;">Agregar mantenimiento</a>
                             </div>
 
                             @if(session('success'))
